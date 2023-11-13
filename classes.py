a0f5340ff776918115e0b4744169fa1ee34a922d
Richard Markus Tins <richar03@ut.ee>
2023-11-13 23:30:14 +0200

    Reworked classes
    Reworked classes, added sprites
    made use of spritegroups
    
    TODO:
    add things to shoot
    make obstacles made of tons of individual sprites

--- classes.py
+++ classes.py
@@ -1,35 +1,82 @@
 import sys
 import pygame as pg
 
-class bullet():
-    x = 0
-    y = 0
-    w = 0
-    h = 0
-    time = None
-    dead = False
+class Bullet(pg.sprite.Sprite):
+    speed = 10
+    def __init__(self,pos):
+        super().__init__()
+        self.image = pg.Surface((pos[0],pos[1]))
+        self.image = pg.image.load("./graphics/bullet.png").convert_alpha()
+        self.rect = self.image.get_rect(center=pos)
     
-    def __init__(self,x1,y1,w1,h1,time = None):
-        self.x = x1
-        self.y = y1
-        self.w = w1
-        self.h = h1
-        self.time = time
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.kill()
+
+class Game():
+    #save window w/h to class local arg
+    width = 0
+    height = 0
+    def __init__(self,w,h):
+        self.width = w
+        self.height = h
+        p_sprite = Player((self.width/2 ,self.height))
+        self.player = pg.sprite.GroupSingle(p_sprite)
     
-    def update_params(self,x1,y1,w1,h1,):
-        self.x = x1
-        self.y = y1
-        self.w = w1
-        self.h = h1
     
-    def draw(self,screen):
-        self.image = pg.draw.rect(screen, (0,255,0), (self.x,self.y,self.w,self.h))
-    
-    def update(self,tk,kt = 250):
-        if self.time is not None:
-            #if kill_time ms have passed, kill instance of object
-            self.kill_time = kt    #200ms
-            if tk - self.time >= self.kill_time:
-                #self.kill()
-                self.dead = True
+    def run(self,screen):
+        self.player.sprite.bullet_group.update()
+        self.player.sprite.bullet_group.draw(screen)
+        self.player.draw(screen)
+        self.player.update()
+
+class Player(pg.sprite.Sprite):
+    #How many pixels it moves / frame
+    speed = 5
+    pos = None
+    ready = True
+    time = 0
+    cooldown = 300
+    bullet_group = None
+    def __init__(self,pos):
+        super().__init__()
+        self.pos = (pos[0]*2,float(pos[1]))
+        self.image = pg.image.load("./graphics/player.png").convert_alpha()
+        self.rect = self.image.get_rect(midbottom=pos)
         
+        self.bullet_group = pg.sprite.Group()
+    
+    def move(self):
+        k = pg.key.get_pressed()
+        if k[pg.K_RIGHT] and (self.rect.x + self.speed < self.pos[0]-32): # minus sprite width
+            self.rect.x += self.speed
+        if k[pg.K_LEFT] and (self.rect.x - self.speed > 0):
+            self.rect.x -= self.speed
+        if k[pg.K_SPACE] and self.ready:
+            self.ready = False
+            self.time = pg.time.get_ticks()
+            self.shoot()
+
+    def charge(self):
+        if not self.ready:
+            time = pg.time.get_ticks()
+            if time -self.time > self.cooldown:
+                self.ready = True
+    
+    def shoot(self):
+        #create bullet at player current middle position
+        self.bullet_group.add(Bullet(self.rect.center))
+    
+    def update(self):
+        self.move()
+        self.charge()
+"""
+class Target(pg.sprite.Sprite):
+    def __init__(self,x,y):
+        x = 0
+        y = 0
+        super().__init__()
+        self.image = pg.image.load("./graphics/alien.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft =(x,y))
+"""
--- movement-test.py
+++ movement-test.py
@@ -3,64 +3,28 @@ from classes import *
 #import time
 # pygame setup
 pg.init()
-screen = pg.display.set_mode((800, 600))
+
+WIDTH = (600)
+HEIGHT = (600)
+
+screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
 running = True
+game = Game(WIDTH,HEIGHT)
 
-r_x = 300
-r_y = 300
-b_trigger = False
-d_trigger = False
+if __name__=="__main__":
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        #tm = round(time.time()*1000,0)   #uses time import
+        screen.fill("black")
+        game.run(screen)
+        
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        pg.display.flip()
 
-bullets = []
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    #tm = round(time.time()*1000,0)   #uses time import
-    screen.fill("black")
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+        clock.tick(60)  # FPS limit
 
-    if event.type == pg.KEYDOWN:
-        if event.key == pg.K_RIGHT:
-            r_x += 5
-        if event.key == pg.K_LEFT:
-            r_x -= 5
-    rect = pg.draw.rect(screen, (0,255,0), (r_x,r_y,50,20))
-    
-    tm = pg.time.get_ticks()
-    #print(tm)
-    if not bool(tm % 20) and b_trigger:
-        b_trigger == False
-        b = bullet(r_x,r_y,5,20,500)
-        bullets.append(b)
-    else:
-        b_trigger = True
-     
-    print(len(bullets))
-    
-    tm = pg.time.get_ticks()
-    for b in bullets:
-        b.update_params(b.x,b.y - 5,b.w,b.h)
-        b.draw(screen)
-    
-    if not bool(tm % 100) and d_trigger and len(bullets) > 5:
-        del bullets[0:5]
-        d_trigger = False
-    else:
-        d_trigger = True
-    
-    
-    # fill the screen with a color to wipe away anything from last frame
-    
-    
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
-    pg.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
-pg.quit()
+    pg.quit()
